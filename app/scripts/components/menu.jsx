e8df1cc35e7ea84573c0dd4e3acbc5eceac3bca6
dylan-gregory <jdylangregory@gmail.com>
2017-02-24 13:17:07 -0500

    Order is being added to local storage

--- app/scripts/components/menu.jsx
+++ app/scripts/components/menu.jsx
@@ -1,42 +1,58 @@
 var React = require('react');
 var Backbone = require('backbone');
+var _ = require ('underscore');
 
 var FoodCollection = require('../models/models.js').FoodCollection;
 var OrderCollection = require('../models/models.js').OrderCollection;
+var Food = require('../models/models.js').Food;
 
 var MenuContainer = React.createClass({
   getInitialState: function(){
     var foodCollection = new FoodCollection();
+    console.log(localStorage.getItem('cartedItems'));
 
     var orderCollection = new OrderCollection();
+    orderCollection.fetch();
+    // var newSubTotal = this.state.orderCollection.addTotal();
+    var total = orderCollection.addTotal();
+
+    // var cartTotal = localStorage.getItem('cartedItems');
 
     return {
       foodCollection: foodCollection,
-      orderCollection: orderCollection
+      orderCollection: orderCollection,
+      subTotal: total
     };
   },
   componentWillMount: function(){
+
     var newFoodCollection = this.state.foodCollection;
 
     newFoodCollection.add([
-      {title: 'Spring Roll', price: 1.75, description: "Lovely, crispy, fried. You can't not eat 12", url: 'https://www.mmfoodmarket.com/images/default-source/default-album/chicken-spring-rolls.jpg?sfvrsn=0'},
-      {title: 'Spicy Basil Fried Rice', price: 8.75, description: "Rice. That is fried. And spicy. What's not to love?", url: 'http://static.wixstatic.com/media/7dc740_fe3351c3a9f449ed87a59aeacc4f4137.jpg'},
-      {title: 'Pad Thai', price: 7.75, description: "Noodley, shrimpy goodness", url: 'http://rasamalaysia.com/wp-content/uploads/2016/02/pad-thai-thumb.jpg'},
-      {title: 'Red Curry', price: 9.25, description: "Creamy red curry sauce over chicken and rice", url: 'http://www.chichilicious.com/wp-content/uploads/Thai-red-curry-recipe.jpg'}
+      {title: 'Spring Roll', price: 1.49, description: "Lovely, crispy, fried. You can't not eat 12", url: 'https://www.mmfoodmarket.com/images/default-source/default-album/chicken-spring-rolls.jpg?sfvrsn=0'},
+      {title: 'Spicy Basil Fried Rice', price: 8.99, description: "Rice. That is fried. And spicy. What's not to love?", url: 'http://static.wixstatic.com/media/7dc740_fe3351c3a9f449ed87a59aeacc4f4137.jpg'},
+      {title: 'Pad Thai', price: 7.79, description: "Noodley, shrimpy goodness", url: 'http://rasamalaysia.com/wp-content/uploads/2016/02/pad-thai-thumb.jpg'},
+      {title: 'Red Curry', price: 9.29, description: "Creamy red curry sauce over chicken and rice", url: 'http://www.chichilicious.com/wp-content/uploads/Thai-red-curry-recipe.jpg'}
     ]);
 
-    this.setState({foodCollection: newFoodCollection});
+    this.setState({foodCollection: newFoodCollection,
+                    });
+
+  },
+  componentDidMount: function(){
+    // var newSubTotal = this.state.orderCollection.addTotal();
+    // console.log(this.state.orderCollection);
 
   },
   addToCart: function(food){
-    console.log(food.get('price'));
     var newOrder = this.state.orderCollection;
-    newOrder.add(food);
-
+    newOrder.create(food.toJSON());
     this.setState({orderCollection: newOrder})
 
-    console.log(newOrder);
-    // food.get('price')
+    var newSubTotal = this.state.orderCollection.addTotal();
+
+    this.setState({subTotal: newSubTotal});
+    console.log(this.state.subTotal);
   },
   render: function(){
 
@@ -59,6 +75,8 @@ var MenuContainer = React.createClass({
 
                   <CartList
                     orderCollection={this.state.orderCollection}
+                    subTotal={this.state.subTotal}
+
                   />
 
                 </div>
@@ -66,7 +84,7 @@ var MenuContainer = React.createClass({
             </div>
           </div>
         <div className="footer">
-          Cool links, logos, yada yada yada...
+          Cool links to yelp, openTable, logos, yada yada yada...
         </div>
       </div>
     );
@@ -108,11 +126,14 @@ var MenuList = React.createClass({
 var CartList = React.createClass({
   getInitialState: function(){
     var cartItems = this.props.orderCollection;
+    console.log('cart-items', this.props.orderCollection);
     return {cartItems: cartItems};
-console.log(this.props.orderCollection);
+
+// console.log(this.props.orderCollection);
   },
   render: function(){
     var self = this;
+    console.log(this.props.subTotal);
     var cartItemList = this.props.orderCollection.map(function(cartItem){
       return (
         <div key={cartItem.cid}>
@@ -129,6 +150,9 @@ console.log(this.props.orderCollection);
         <ul>
           {cartItemList}
         </ul>
+        <div>Subtotal:
+          <span>${this.props.subTotal==0 ? "0.00" : this.props.subTotal}</span>
+        </div>
         <OrderForm />
       </div>
     );
@@ -136,11 +160,22 @@ console.log(this.props.orderCollection);
 });
 
 var OrderForm = React.createClass({
+  handleName: function(event){
+    this.setState({name: event.target.value});
+  },
+  handleNumber: function(event){
+    this.setState({number: event.target.value});
+  },
+  handleSubmit: function(event){
+    event.preventDefault();
+
+
+  },
   render: function(){
     return (
-      <form>
-        <input type="text" placeholder="Your Name" />
-        <input type="text" placeholder="Your Phone Number" />
+      <form onSubmit={this.handleSubmit}>
+        <input type="text" onChange={this.handleName} placeholder="Your Name" />
+        <input type="text" onChange={this.handleNumber} placeholder="Your Phone Number" />
 
 
         <button className="btn btn-success">Place my order!</button>
--- app/scripts/models/models.js
+++ app/scripts/models/models.js
@@ -1,7 +1,8 @@
 var Backbone = require('backbone');
+Backbone.LocalStorage = require('backbone.localstorage');
 
 var Food = Backbone.Model.extend({
-
+  // idAttribute: '_id'
 });
 
 var FoodCollection = Backbone.Collection.extend({
@@ -11,17 +12,21 @@ var FoodCollection = Backbone.Collection.extend({
 // this will be for the items added into the cart
 var OrderCollection = Backbone.Collection.extend({
   model: Food,
+  localStorage: new Backbone.LocalStorage('cartedItems'),
   addTotal: function(){
     var cartItemPrices = this.map(function(item){
-      return item.price
+      return item.get('price');
     });
 
     var subtotal = cartItemPrices.reduce(function(memo, num){
       return memo + num;
     });
-    return subtotal;
+    // console.log(cartItemPrices);
+    return subtotal.toFixed(2);
   }
 });
+
+
 //will call this wherever I need the subtotal to be show like this.props.orderCollection.subtotal()
 
 module.exports = {
