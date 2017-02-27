0aeea0b2db7ab254494672066a7b0cbfc65161ee
dylan-gregory <jdylangregory@gmail.com>
2017-02-27 10:10:18 -0500

    publish: updates package.json
    generated from commit ef974b4ee40f37c856ab79e9166a60dc38330e62

--- app/scripts/index.js
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-var MenuContainer = require('./components/menu.jsx').MenuContainer;
-
-ReactDOM.render(
-  React.createElement(MenuContainer),
-  document.getElementById('app')
-);
