77e8968614ae04d207071271e6dedfc3155ea8f8
dylan-gregory <jdylangregory@gmail.com>
2017-02-26 20:54:57 -0500

    Styled, modal working

--- app/scripts/components/menu.jsx
+++ app/scripts/components/menu.jsx
@@ -1,6 +1,11 @@
+var $ = window.$ = window.jQuery = require('jquery');
 var React = require('react');
+var ReactDOM = require('react-dom');
 var Backbone = require('backbone');
 var _ = require ('underscore');
+require('bootstrap-sass');
+var Modal = require('react-bootstrap').Modal;
+var Button = require('react-bootstrap').Button;
 
 var FoodCollection = require('../models/models.js').FoodCollection;
 var OrderCollection = require('../models/models.js').OrderCollection;
@@ -12,24 +17,23 @@ var SentOrderCollection = require('../models/models.js').SentOrderCollection;
 var MenuContainer = React.createClass({
   getInitialState: function(){
     var foodCollection = new FoodCollection();
-    // console.log(localStorage.getItem('cartedItems'));
     var sentOrderCollection = new SentOrderCollection();
     var orderCollection = new OrderCollection();
     console.log(orderCollection);
     orderCollection.fetch();
-    // var newSubTotal = this.state.orderCollection.addTotal();
+
     if (localStorage.getItem('cartedItems') == null || localStorage.getItem('cartedItems').length == 0) {
       var total = 0;
     }else{
     var total = orderCollection.addTotal();
     }
-    // var cartTotal = localStorage.getItem('cartedItems');
 
     return {
       foodCollection: foodCollection,
       orderCollection: orderCollection,
       sentOrderCollection: sentOrderCollection,
-      subTotal: total
+      subTotal: total,
+      showModal: false
     };
   },
   componentWillMount: function(){
@@ -37,10 +41,14 @@ var MenuContainer = React.createClass({
     var newFoodCollection = this.state.foodCollection;
 
     newFoodCollection.add([
-      {title: 'Spring Roll', price: 1.49, description: "Lovely, crispy, fried. You can't not eat 12", url: 'https://www.mmfoodmarket.com/images/default-source/default-album/chicken-spring-rolls.jpg?sfvrsn=0'},
-      {title: 'Spicy Basil Fried Rice', price: 8.99, description: "Rice. That is fried. And spicy. What's not to love?", url: 'http://static.wixstatic.com/media/7dc740_fe3351c3a9f449ed87a59aeacc4f4137.jpg'},
-      {title: 'Pad Thai', price: 7.79, description: "Noodley, shrimpy goodness", url: 'http://rasamalaysia.com/wp-content/uploads/2016/02/pad-thai-thumb.jpg'},
-      {title: 'Red Curry', price: 9.29, description: "Creamy red curry sauce over chicken and rice", url: 'http://www.chichilicious.com/wp-content/uploads/Thai-red-curry-recipe.jpg'}
+      {title: 'Spring Roll', price: 1.49, description: "Lovely, crispy, fried vegetable spring roll, served with sweet red chili dipping sauce. You can't not eat 12", url: 'https://www.mmfoodmarket.com/images/default-source/default-album/chicken-spring-rolls.jpg?sfvrsn=0'},
+      {title: 'Spicy Basil Fried Rice', price: 8.99, description: "Rice. That is fried. And spicy. What's not to love? Served with your choice of chicken, shrimp, or beef", url: 'http://static.wixstatic.com/media/7dc740_fe3351c3a9f449ed87a59aeacc4f4137.jpg'},
+      {title: 'Pad Thai', price: 7.79, description: "Our take on the classic street dish - made with housemade noodles, special sauce, peanuts, mixed vegetabes, and your choice of chicken or shrimp", url: 'http://rasamalaysia.com/wp-content/uploads/2016/02/pad-thai-thumb.jpg'},
+      {title: 'Red Curry', price: 9.29, description: "Creamy red curry sauce over chicken, rice, and mixed vegetables", url: 'http://www.chichilicious.com/wp-content/uploads/Thai-red-curry-recipe.jpg'},
+      {title: 'Yellow Curry', price: 9.49, description: "Like the Red curry...but yellow. Creamy yellow curry sauce over chicken, rice, and mixed vegetables", url: 'http://keeprecipes.com/sites/keeprecipes/files/yellow-chicken-curry.jpg'},
+      {title: 'Tom Kha Gai', price: 8.79, description: "Coconut milk based soup, made with chicken, lemongrass, mushrooms, and chilies", url: 'http://assets.bonappetit.com/photos/57ae064653e63daf11a4e155/16:9/w_1000,c_limit/tom-kha-gai-chicken-coconut-soup.jpg'},
+      {title: 'Orange Chicken', price: 7.79, description: "Crispy fried chicken smothered in our tangy orange sauce - served over rice and vegetables", url: 'https://i.ytimg.com/vi/W6OHCkpbdmo/maxresdefault.jpg'},
+      {title: 'Thai Iced Tea', price: 2.29, description: "Sweet, spicy, and very refreshing", url: 'http://images.tastespotting.com/thumbnails/307173.jpg'}
     ]);
 
     this.setState({foodCollection: newFoodCollection,
@@ -69,14 +77,25 @@ var MenuContainer = React.createClass({
     this.setState({subTotal: newSubTotal});
   },
   sendOrder: function(order){
-    console.log("new order", order);
     var orderToSend = this.state.sentOrderCollection;
     orderToSend.create(order);
     this.setState({sentOrderCollection: orderToSend});
 
-    this.setState({orderCollection: new OrderCollection(), sentOrderCollection: '' , subTotal: 0});
+    this.setState({orderCollection: new OrderCollection(), sentOrderCollection: new SentOrderCollection() , subTotal: 0});
     localStorage.clear();
 
+
+  },
+  openModal: function(){
+    this.setState({showModal: true});
+
+    var confirm = new ConfirmationModal();
+    return this.setState({showModal: true});
+
+  },
+  closeModal: function(){
+
+    return this.setState({showModal: false});
   },
   render: function(){
 
@@ -89,7 +108,7 @@ var MenuContainer = React.createClass({
 
 
         </div>
-          <div className="container">
+          <div id="container" className="container body" data-spy="scroll" data-target=".scrollspy">
             <div className="row">
             </div>
             <div className="row">
@@ -98,6 +117,9 @@ var MenuContainer = React.createClass({
                 <MenuList foodCollection={this.state.foodCollection}
                           addToCart={this.addToCart}
                 />
+              <ConfirmationModal openModal={this.state.showModal}
+              closeModal={this.closeModal}
+              />
 
               </div>
               <div className="col-md-4">
@@ -108,6 +130,7 @@ var MenuContainer = React.createClass({
                     subTotal={this.state.subTotal}
                     deleteFromOrder={this.deleteFromOrder}
                     sendOrder={this.sendOrder}
+                    openModal={this.openModal}
 
                   />
 
@@ -116,11 +139,11 @@ var MenuContainer = React.createClass({
             </div>
           </div>
         <div className="footer row">
-          <div className="col-md-4 yelp-note"><div>How was your meal? Let us know on Yelp</div><i className="fa fa-yelp" aria-hidden="true"></i></div>
+          <div className="col-md-4 yelp-note"><div>How was your meal? Let us know on Yelp</div><a href="https://www.yelp.com/greenville"><i className="fa fa-yelp" aria-hidden="true"></i></a></div>
           <div className="col-md-4 social-notes"><div>Want to connect?</div>
-          <i className="fa fa-instagram" aria-hidden="true"></i>
-          <i className="fa fa-facebook-square" aria-hidden="true"></i>
-          <i className="fa fa-twitter-square" aria-hidden="true"></i></div>
+          <a href="https://www.instagram.com/?hl=en"><i className="fa fa-instagram" aria-hidden="true"></i></a>
+          <a href="https://www.facebook.com/"><i className="fa fa-facebook-square" aria-hidden="true"></i></a>
+          <a href="https://twitter.com/?lang=en"><i className="fa fa-twitter-square" aria-hidden="true"></i></a></div>
           <div className="col-md-4 lemon"><div>(And here's a bonus little lemon)</div> <i className="fa fa-lemon-o" aria-hidden="true"></i></div>
 
         </div>
@@ -182,7 +205,6 @@ var CartList = React.createClass({
   // },
   render: function(){
     var self = this;
-    console.log(this.props.subTotal);
     var cartItemList = this.props.orderCollection.map(function(cartItem){
       return (
         <div className="cart-item row" key={cartItem.cid}>
@@ -203,7 +225,7 @@ var CartList = React.createClass({
     });
 
     return (
-      <div className="cart-orders well">
+      <div className="cart-orders well scrollspy">
         <h2>Your Order:</h2>
         <ul className="cart-list">
           {cartItemList}
@@ -211,7 +233,7 @@ var CartList = React.createClass({
         <div className="cart-subtotal well">Subtotal:
           <span className="subtotal-price">${this.props.subTotal==0 ? "0.00" : this.props.subTotal}</span>
         </div>
-        <OrderForm orderCollection={this.props.orderCollection}
+        <OrderForm  openModal={this.props.openModal} orderCollection={this.props.orderCollection}
           sendOrder={this.props.sendOrder}/>
       </div>
     );
@@ -235,6 +257,9 @@ var OrderForm = React.createClass({
     this.props.sendOrder(this.state);
 
   },
+  openModal: function(){
+    this.props.openModal();
+  },
   render: function(){
 
     return (
@@ -243,11 +268,57 @@ var OrderForm = React.createClass({
         <input type="text" className="form-control order-input" onChange={this.handleNumber} placeholder="Your Phone Number" />
 
 
-        <button className="btn btn-success">Place my order!</button>
+        <button onClick={this.openModal} className="btn btn-success">Place my order!</button>
       </form>
     );
   }
 });
+
+
+
+var ConfirmationModal = React.createClass({
+  getInitialState() {
+    return {showModal: false};
+  },
+  componentWillReceiveProps: function(openModal) {
+  this.setState({showModal: this.props.openModal});
+  // this.setState({showModal: false});
+  },
+  close() {
+    this.props.closeModal();
+    this.setState({ showModal: false });
+  },
+
+  open() {
+    this.setState({ showModal: true });
+  },
+  openModal() {
+    //
+    // this.props.openModal();
+    // this.setState({showModal: this.props.openModal});
+
+  },
+
+  render() {
+
+    return (
+      <div>
+        <Modal show={this.state.showModal} onHide={this.close}>
+          <Modal.Header className="thank-you" closeButton>
+            <Modal.Title ><span className="thanks" >Thank you for your order!</span></Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p className="food-ready"><i className="fa fa-cutlery" aria-hidden="true"></i>Your food will be ready in 15-20 minutes. See you soon!</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button className="btn btn-danger" onClick={this.close}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    );
+  }
+});
+
 
 module.exports = {
   MenuContainer
--- app/scripts/router.js
+++ app/scripts/router.js
@@ -3,5 +3,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var AppRouter = Backbone.Router.extend({
+  routes:{
+    '': 'index',
 
+  }
 });
